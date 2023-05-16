a7ba67f8943543e5fbabea4dea937013ee12e344
GuilhermeRodrigues22 <guicordeiro556@gmail.com>
2023-05-16 16:38:23 -0300

    iniciado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EstudanteComponent } from './estudante/estudante.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudanteComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/estudantes.ts
@@ -0,0 +1,8 @@
+export interface Aluno {
+  id: number;
+  name: string;
+  email: string;
+  telefone: number;
+  idade: number;
+  rg: number;
+}
